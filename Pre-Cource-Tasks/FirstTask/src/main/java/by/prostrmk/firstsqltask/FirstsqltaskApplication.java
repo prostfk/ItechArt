e3f59027c6eb2493f55c1e8a57acd983d138bb6b
prostfk@gmail.com <prostfk@gmail.com>
2018-07-25 13:08:31 +0300

    init

---
+++ Pre-Cource-Tasks/FirstTask/src/main/java/by/prostrmk/firstsqltask/FirstsqltaskApplication.java
@@ -0,0 +1,12 @@
+package by.prostrmk.firstsqltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstsqltaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstsqltaskApplication.class, args);
+    }
+}
