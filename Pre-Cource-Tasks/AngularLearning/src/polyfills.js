3b0e6cf589010427930ae2731ac109b6639f8109
prostfk@gmail.com <prostfk@gmail.com>
2018-07-30 23:30:12 +0300

    done hello world on angular

---
+++ Pre-Cource-Tasks/AngularLearning/src/polyfills.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("core-js/es6");
+// для поддержки Reflect Api
+require("core-js/es7/reflect");
+// zone используется angular
+require("zone.js/dist/zone");
