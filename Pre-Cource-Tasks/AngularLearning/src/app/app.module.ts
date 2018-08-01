afda414697efde1a66d28204ca5d717c7dd565b4
prostfk@gmail.com <prostfk@gmail.com>
2018-08-01 14:36:03 +0300

    add routes

--- Pre-Cource-Tasks/AngularLearning/src/app/app.module.ts
+++ Pre-Cource-Tasks/AngularLearning/src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {UserComponent} from "./user/user.component";
+import {HomePageComponent} from './home-page/home-page.component';
+import {SetupPageComponent} from './setup-page/setup-page.component';
+import {RouterModule} from "@angular/router";
+
+
+const routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'setup', component: SetupPageComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    HomePageComponent,
+    SetupPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
