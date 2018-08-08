7621948fe9f28cbe420b318aa1cd860e0d14917e
prostfk@gmail.com <prostfk@gmail.com>
2018-08-08 14:31:44 +0300

    finish

--- Pre-Cource-Tasks/FirstTask/src/main/java/by/prostrmk/firstsqltask/controller/IndexController.java
+++ Pre-Cource-Tasks/FirstTask/src/main/java/by/prostrmk/firstsqltask/controller/IndexController.java
@@ -19,61 +19,62 @@ public class IndexController {
     @Autowired
     StudentsDao studentsDao;
 
-    @RequestMapping(value = "/1",method = RequestMethod.GET)
-    public ModelAndView getIndexPage(){
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public ModelAndView getAllStudents(){
         List<Student> all = studentsDao.findAll();
+        return new ModelAndView("index", "users",all);
+    }
+
+    @RequestMapping(value = "/1",method = RequestMethod.GET)
+    public ModelAndView studentsWhoPassedExams(){
+        List<String> all = studentsDao.findStudentsWhoPassedExams();
         return new ModelAndView("index","users", all);
     }
 
     @RequestMapping(value = "/2", method = RequestMethod.GET)
-    public ModelAndView passedStudents(){
-        List<String> studentsWhoPassedExams = studentsDao.findStudentsWhoPassedExams();
+    public ModelAndView countOfGoodStudents(){
+        List<String> studentsWhoPassedExams = Collections.singletonList("Count Of Good Students: " + studentsDao.findCountOfGoodStudents());
         return new ModelAndView("index", "users", studentsWhoPassedExams);
     }
 
     @RequestMapping(value = "/3",method = RequestMethod.GET)
-    public ModelAndView goodStudents(){
-        return new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Count of good students: " + studentsDao.findCountOfGoodStudents())));
+    public ModelAndView countOfStudentWhoPassedExamAutomatically(){
+        return new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Count Of Student Who Passed Exam Automatically: " + studentsDao.findCountOfStudentWhoPassedExamAutomatically())));
     }
 
     @RequestMapping(value = "/4",method = RequestMethod.GET)
-    public ModelAndView studentsWhoPassedAutomatically(){
-        return new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Count of good students: " + studentsDao.findCountOfStudentWhoPassedExamAutomatically())));
+    public ModelAndView middleValueOfDataBaseManagement(){
+        return new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Middle Value Of Data Base Management: " + studentsDao.findMiddleValueOfDataBaseManagement())));
     }
 
     @RequestMapping(value = "/5",method = RequestMethod.GET)
-    public ModelAndView middleValueOfDBManagement(){
-        return new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Middle value: " + studentsDao.findMiddleValueOfDataBaseManagement())));
+    public ModelAndView studentsWhoDidNotPassTheExam(){
+        return new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Students Who Did Not Pass The Exam: " + studentsDao.findStudentsWhoDidNotPassTheExam())));
     }
 
     @RequestMapping(value = "/6",method = RequestMethod.GET)
-    public ModelAndView studentsWhoDidNotPass(){
-        return new ModelAndView("index", "users",studentsDao.findStudentsWhoDidNotPassTheExam());
+    public ModelAndView teacherWhoProfessMoreThanOneCourse(){
+        return new ModelAndView("index", "users",studentsDao.findTeacherWhoProfessMoreThanOneCourse());
     }
 
     @RequestMapping(value = "/7", method = RequestMethod.GET)
-    public ModelAndView smartTeacher(){
-        return new ModelAndView("index", "users", studentsDao.findTeacherWhoProfessMoreThanOneCourse());
-    }
-
-    @RequestMapping(value = "/8", method = RequestMethod.GET)
-    public ModelAndView personsThoRetried(){
+    public ModelAndView peopleWhoRetriedToPassExam(){
         return new ModelAndView("index", "users", studentsDao.findPeopleWhoRetriedToPassExam());
     }
 
-    @RequestMapping(value = "/topFive", method = RequestMethod.GET)
+    @RequestMapping(value = "/8", method = RequestMethod.GET)
     public ModelAndView fiveSmartest(){
         return new ModelAndView("index", "users", studentsDao.findFiveSmartest());
     }
 
-    @RequestMapping(value = "/10", method = RequestMethod.GET)
-    public ModelAndView topOnMath(){
-        return new ModelAndView("index", "users", studentsDao.findSmartestInMath());
+    @RequestMapping(value = "/9", method = RequestMethod.GET)
+    public ModelAndView bestTeacher(){
+        return new ModelAndView("index", "users", studentsDao.findBestTeacher());
     }
 
-    @RequestMapping(value = "/9", method = RequestMethod.GET)
-    public ModelAndView topTeacher(){
-        return new ModelAndView("index", "users", studentsDao.findBestTeacher());
+    @RequestMapping(value = "/10", method = RequestMethod.GET)
+    public ModelAndView smartestInMath(){
+        return new ModelAndView("index", "users", studentsDao.findSmartestInMath());
     }
 
 
