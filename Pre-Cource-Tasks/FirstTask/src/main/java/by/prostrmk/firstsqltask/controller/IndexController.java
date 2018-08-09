431db42dd9696aa65fd9d1c319e27532fc399af3
prostrmk <prostrmk@gmail.com>
2018-08-09 19:07:07 +0300

    remove hardcode

--- Pre-Cource-Tasks/FirstTask/src/main/java/by/prostrmk/firstsqltask/controller/IndexController.java
+++ Pre-Cource-Tasks/FirstTask/src/main/java/by/prostrmk/firstsqltask/controller/IndexController.java
@@ -4,6 +4,7 @@ import by.prostrmk.firstsqltask.model.dao.StudentsDao;
 import by.prostrmk.firstsqltask.model.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,62 +21,52 @@ public class IndexController {
     StudentsDao studentsDao;
 
     @RequestMapping(value = {"/all", "/"}, method = RequestMethod.GET)
-    public ModelAndView getAllStudents(){
+    public ModelAndView getAllStudents() {
         List<Student> all = studentsDao.findAll();
-        return new ModelAndView("index", "users",all);
+        return new ModelAndView("index", "users", all);
     }
 
-    @RequestMapping(value = "/1",method = RequestMethod.GET)
-    public ModelAndView studentsWhoPassedExams(){
-        List<String> all = studentsDao.findStudentsWhoPassedExams();
-        return new ModelAndView("index","users", all);
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ModelAndView executeQuery(@PathVariable String id) {
+        ModelAndView modelAndView = null;
+        switch (id) {
+            case "1":
+                modelAndView = new ModelAndView("index", "users", Collections.singletonList("Count Of Good Students: " + studentsDao.findCountOfGoodStudents()));
+                break;
+            case "2":
+                modelAndView = new ModelAndView("index", "users", Collections.singletonList("Count Of Good Students: " + studentsDao.findCountOfGoodStudents()));
+                break;
+            case "3":
+                modelAndView = new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Count Of Student Who Passed Exam Automatically: " + studentsDao.findCountOfStudentWhoPassedExamAutomatically())));
+                break;
+            case "4":
+                modelAndView = new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Middle Value Of Data Base Management: " + studentsDao.findMiddleValueOfDataBaseManagement())));
+                break;
+            case "5":
+                modelAndView = new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Students Who Did Not Pass The Exam: " + studentsDao.findStudentsWhoDidNotPassTheExam())));
+                break;
+            case "6":
+                modelAndView = new ModelAndView("index", "users", studentsDao.findTeacherWhoProfessMoreThanOneCourse());
+                break;
+            case "7":
+                modelAndView = new ModelAndView("index", "users", studentsDao.findPeopleWhoRetriedToPassExam());
+                break;
+            case "8":
+                modelAndView = new ModelAndView("index", "users", studentsDao.findFiveSmartest());
+                break;
+            case "9":
+                modelAndView = new ModelAndView("index", "users", studentsDao.findBestTeacher());
+                break;
+            case "10":
+                modelAndView = new ModelAndView("index", "users", studentsDao.findSmartestInMath());
+                break;
+            default:
+                modelAndView = new ModelAndView("index", "users", "error");
+                break;
+        }
+        return modelAndView;
     }
 
-    @RequestMapping(value = "/2", method = RequestMethod.GET)
-    public ModelAndView countOfGoodStudents(){
-        List<String> studentsWhoPassedExams = Collections.singletonList("Count Of Good Students: " + studentsDao.findCountOfGoodStudents());
-        return new ModelAndView("index", "users", studentsWhoPassedExams);
-    }
-
-    @RequestMapping(value = "/3",method = RequestMethod.GET)
-    public ModelAndView countOfStudentWhoPassedExamAutomatically(){
-        return new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Count Of Student Who Passed Exam Automatically: " + studentsDao.findCountOfStudentWhoPassedExamAutomatically())));
-    }
-
-    @RequestMapping(value = "/4",method = RequestMethod.GET)
-    public ModelAndView middleValueOfDataBaseManagement(){
-        return new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Middle Value Of Data Base Management: " + studentsDao.findMiddleValueOfDataBaseManagement())));
-    }
-
-    @RequestMapping(value = "/5",method = RequestMethod.GET)
-    public ModelAndView studentsWhoDidNotPassTheExam(){
-        return new ModelAndView("index", "users", new ArrayList<String>(Collections.singleton("Students Who Did Not Pass The Exam: " + studentsDao.findStudentsWhoDidNotPassTheExam())));
-    }
-
-    @RequestMapping(value = "/6",method = RequestMethod.GET)
-    public ModelAndView teacherWhoProfessMoreThanOneCourse(){
-        return new ModelAndView("index", "users",studentsDao.findTeacherWhoProfessMoreThanOneCourse());
-    }
-
-    @RequestMapping(value = "/7", method = RequestMethod.GET)
-    public ModelAndView peopleWhoRetriedToPassExam(){
-        return new ModelAndView("index", "users", studentsDao.findPeopleWhoRetriedToPassExam());
-    }
-
-    @RequestMapping(value = "/8", method = RequestMethod.GET)
-    public ModelAndView fiveSmartest(){
-        return new ModelAndView("index", "users", studentsDao.findFiveSmartest());
-    }
-
-    @RequestMapping(value = "/9", method = RequestMethod.GET)
-    public ModelAndView bestTeacher(){
-        return new ModelAndView("index", "users", studentsDao.findBestTeacher());
-    }
-
-    @RequestMapping(value = "/10", method = RequestMethod.GET)
-    public ModelAndView smartestInMath(){
-        return new ModelAndView("index", "users", studentsDao.findSmartestInMath());
-    }
 
 
 }
