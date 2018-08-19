1626e56e1abb750d9a8ec70d678cc972755125cb
prostfk <prostfk@gmail.com>
2018-08-19 16:47:58 +0300

    add some routes

--- Pre-Cource-Tasks/ThirdTaskNode/index.js
+++ Pre-Cource-Tasks/ThirdTaskNode/index.js
@@ -3,35 +3,77 @@ const express =require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const app = express();
-
+const artists = [
+    {
+        id: 1,
+        name: "bfmv"
+    },
+    {
+        id:2,
+        name: "annisokay"
+    },
+    {
+        id:3,
+        name: "a day to remember"
+    },
+    {
+        id:4,
+        name: "wildways"
+    }
+];
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     extname: '.hbs',
     layoutsDir: path.join(__dirname, 'views/layouts')
 }));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-// app.use((request,response, next) =>{
-//    console.log(request.headers);
-//    next();
-// });
-//
-// app.use((request, response, next) =>{
-//    request.chance = Math.random();
-//    next();
-// });
-//
 app.get('/', (request,response) =>{
    response.render('home',{
        name: 'Roman'
    })
 });
-//
-// app.use((err, request, response, next) => {
-//     console.log(err);
-//     response.status(500).send('Something broke!');
-// });
+
+app.get('/alarms',(request, response) =>{
+   response.render('alarm',{
+       message: 'hello '
+   })
+});
+
+app.get('/artists', (request, response)=>{
+   response.send(artists)
+});
+
+app.get('/artists/:id', (request,response) =>{
+    console.log(request.params.id);
+    let artist = artists.find(function (artist) {
+       return artist.id === Number(request.params.id)
+    });
+    console.log(artist);
+    response.render('alarm', {
+        message: artist.name
+    })
+});
+
+app.get('/addArtist', (req,resp)=>{
+
+    resp.render('addArtist')
+
+});
+
+app.post('/addArtist', (req,resp)=>{
+   let ar = req.params.artist;
+   artists.push({
+       id:5,
+       name: ar.toString()
+   });
+    resp.send("Success");
+});
+
 
 app.listen(config.port);
